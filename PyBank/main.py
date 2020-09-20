290d55f509da4bdad5b111ad53e80ad9c2bccd40
eggsnbacon97 <danieljconrad@protonmail.com>
2020-09-19 20:36:50 -0400

    Months and total are now populating properly - no output to text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,16 @@
 import os
 import csv
 
+#from os.path import expanduser
+#home = expanduser("~")
+#budgetdata = os.path.join(home,'C:/Users/Dan/BDAB/python-challenge/PyBank/Resources/' )
+
 path= 'C:/Users/Dan/BDAB/python-challenge/PyBank/Resources/'
 budgetdata=os.path.join(path, '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
 monthtotal=0
+pltotal=0
+
 
 with open(budgetdata, newline='') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
@@ -14,5 +20,7 @@ with open(budgetdata, newline='') as csvfile:
 
     for row in csvreader:
         monthtotal += 1 
+        pltotal += int(row[1])
 
 print(f"Total Months: {monthtotal}")
+print(f"Total: ${pltotal}")
